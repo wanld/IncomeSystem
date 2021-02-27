75b829ca251dd0c7ec9a770143d9a35e5e00c2a8
bryantz <71428400+bryantz13@users.noreply.github.com>
2021-02-27 15:28:47 +0700

    Update income.php

--- income.php
+++ income.php
@@ -1,4 +1,5 @@
 <?php 
+<<<<<<< Updated upstream
 echo "Hello, world!";
 echo "KiKi";
 echo "BRyantz";
@@ -6,6 +7,9 @@ echo "J";
 <<<<<<< HEAD
 echo "Doten";
 =======
+=======
+echo "Hello, Bryantz!";
+>>>>>>> Stashed changes
 
 >>>>>>> 153e7790c70a7c8d51722ada0d4be78d1fabf84d
 ?>
